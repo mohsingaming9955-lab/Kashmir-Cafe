300c35f802b4f9f0ff47d1ca32f8b66d2da30d5b
mohsingaming9955-lab <mohsingaming9955@gmail.com>
2026-04-29 16:30:39 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/Hero';
+import Pricing from '@/components/Pricing';
+import ContactForm from '@/components/ContactForm';
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Pricing />
+      <ContactForm />
+    </main>
+  );
+}
